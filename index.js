0add1068d52cb20700c2c22efe68afce965a68b3
forrestmorrison <forrestmorrison@gmail.com>
2022-02-21 10:50:08 -0600

    basic Express

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to our API');
+})
+
+
+app.listen(port, () => {
+    console.log(`web server is listening on port ${port}`);
+})
