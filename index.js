968ed106ee93d870eb0bbecae9b4f3667548cc34
forrestmorrison <forrestmorrison@gmail.com>
2022-02-21 12:32:22 -0600

    route for all employees

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 const port = process.env.PORT || 4001;
 const employees = require('./routes/employees');
 
-app.get('/', (req, res) => {
-    res.send('Welcome to our API');
-})
+// app.get('/', (req, res) => {
+//     res.send('Welcome to our API');
+// })
 
+app.use(employees);
 
 app.listen(port, () => {
     console.log(`web server is listening on port ${port}`);
