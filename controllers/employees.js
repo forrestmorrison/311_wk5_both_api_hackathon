1f4fa3e58fe941f9400a920821236afcb1e8e8b6
forrestmorrison <forrestmorrison@gmail.com>
2022-02-21 15:35:31 -0600

    get employees by id

--- controllers/employees.js
+++ controllers/employees.js
@@ -6,33 +6,44 @@ const getEmployees = (req, res) => {
     let sql = "SELECT * FROM employees LIMIT 50";
 
     pool.query(sql, (err, rows) => {
-        if (err) return handleSQLError(res, err)
+        if (err) return handleSQLError(res, err);
+        return res.json("getting employees..." + rows);
+    })
+}
+
+const getEmployeesById = (req, res) => {
+    let sql = "SELECT * FROM employees WHERE emp_no = ?";
+    sql = mysql.format(sql, [req.params.emp_no]);
+
+    pool.query(sql, (err, rows) => {
+        if (err) return handleSQLError(res, err);
         return res.json("getting employees..." + rows);
     })
 }
 
 const getEmployeesByFirstName = (req, res) => { 
     let sql = "SELECT * FROM employees WHERE first_name = ?";
-    sql = mysql.format(sql, [req.body.first_name])
+    sql = mysql.format(sql, [req.params.first_name]);
     
     pool.query(sql, (err, rows) => {
-        if (err) return handleSQLError(res, err)
+        if (err) return handleSQLError(res, err);
         return res.json("getting employees..." + rows);
     })
 }
 
-const getSalariesById = (req, res) => { 
-    let sql = "SELECT * FROM salaries WHERE emp_no = ?";
-    sql = mysql.format(sql, [req.body.emp_num])
+// const getSalariesById = (req, res) => { 
+//     let sql = "SELECT * FROM salaries WHERE emp_no = ?";
+//     sql = mysql.format(sql, [req.body.emp_no]);
     
-    pool.query(sql, (err, rows) => {
-        if (err) return handleSQLError(res, err)
-        return res.json("getting employee salary data..." + rows);
-    })
-}
+//     pool.query(sql, (err, rows) => {
+//         if (err) return handleSQLError(res, err);
+//         return res.json("getting employee salary data..." + rows);
+//     })
+// }
 
 module.exports = {
     getEmployees,
+    getEmployeesById,
     getEmployeesByFirstName,
-    getSalariesById
+    // getSalariesById
 }
--- routes/employees.js
+++ routes/employees.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 
 router.get('/', employeesController.getEmployees);
+router.get('/:emp_no', employeesController.getEmployeesById);
 router.get('/:first_name', employeesController.getEmployeesByFirstName);
-router.get('/:emp_no', employeesController.getSalariesById);
+// router.get('/salaries/:emp_no', employeesController.getSalariesById);
 
 
 
