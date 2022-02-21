9bf096676dce903f5618c73898fb4d5408efc524
taplinseth <taplinseth@gmail.com>
2022-02-21 12:53:01 -0600

    added ability to search salary data by Id

--- routes/employees.js
+++ routes/employees.js
@@ -3,5 +3,6 @@ const employeesController = require('../controllers/employees')
 const router = express.Router();
 
 router.get('/:first_name', employeesController.getEmployeesByFirstName)
+router.get('/:emp_no', employeesController.getSalariesById)
 
 module.exports = router;
